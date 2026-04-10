7473a466dee6e3a7253ad9d0aca7dafd1e4dd1a4
Hengmin Zhang <52320349+ZhangHengMin@users.noreply.github.com>
2026-04-10 16:53:48 +0800

    upload

---
+++ static/js/scripts.js
@@ -0,0 +1,35 @@
+const content_dir = 'contents/'
+const config_file = 'config.yml'
+
+
+window.addEventListener('DOMContentLoaded', event => {
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+
+    // Yaml
+    fetch(content_dir + config_file)
+        .then(response => response.text())
+        .then(text => {
+            const yml = jsyaml.load(text);
+            Object.keys(yml).forEach(key => {
+                try {
+                    document.getElementById(key).innerHTML = yml[key];
+                } catch {
+                    console.log("Unknown id and value: " + key + "," + yml[key].toString())
+                }
+
+            })
+        })
+        .catch(error => console.log(error));
+}); 
